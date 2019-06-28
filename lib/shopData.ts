e491b9e2902847cde58a199d9bfd4f52df21f035
Nicole <nicole.sattler@live.com>
2019-06-28 14:41:08 -0500

    Add shop data

---
+++ lib/shopData.ts
@@ -0,0 +1,38 @@
+export const serviceList = {
+  1: {
+    service: 'Adult Haircut',
+    price: 25
+  },
+  2: {
+    service: 'Child Haircut',
+    price: 15
+  },
+  3: {
+    service: 'Haircut & Shave',
+    price: 35
+  },
+  4: {
+    service: 'Beard Trim',
+    price: 15
+  },
+  5: {
+    service: 'Dry Shave with Clippers',
+    price: 10
+  },
+  6: {
+    service: 'Razor Shave',
+    price: 15
+  },
+  7: {
+    service: 'Hairline or Edge Up',
+    price: 10
+  },
+  8: {
+    service: 'Mustache Trim',
+    price: 7
+  },
+  9: {
+    service: 'Shampoo',
+    price: 15
+  }
+}
