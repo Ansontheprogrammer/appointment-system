fc232634bfdc9228ae66edc896764f669ccae46b
Anson Ervin <ansonervin@yahoo.com>
2019-07-13 07:41:12 -0400

    Added duration to services

--- lib/shopData.ts
+++ lib/shopData.ts
@@ -2,46 +2,46 @@ export const serviceList = {
   1: {
     service: 'Adult Haircut',
     price: 25,
-    serviceTime: 45
+    duration: 45
   },
   2: {
     service: 'Child Haircut',
     price: 15,
-    serviceTime: 30,
+    duration: 30,
   },
   3: {
     service: 'Haircut & Shave',
     price: 35,
-    serviceTime: 60,
+    duration: 60,
   },
   4: {
     service: 'Beard Trim',
     price: 15,
-    serviceTime: 15,
+    duration: 15,
   },
   5: {
     service: 'Dry Shave with Clippers',
     price: 10,
-    serviceTime: 45
+    duration: 45
   },
   6: {
     service: 'Razor Shave',
     price: 15,
-    serviceTime: 45
+    duration: 45
   },
   7: {
     service: 'Hairline or Edge Up',
     price: 10,
-    serviceTime: 30
+    duration: 30
   },
   8: {
     service: 'Mustache Trim',
     price: 7,
-    serviceTime: 14
+    duration: 14
   },
   9: {
     service: 'Shampoo',
     price: 15,
-    serviceTime: 30
+    duration: 30
   }
 }
