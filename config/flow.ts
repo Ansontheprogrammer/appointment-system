25e21c531de48caba180c97738293041748b439e
Anson Ervin <ansonervin@yahoo.com>
2019-08-12 06:58:16 -0400

    Removed additional services

--- config/flow.ts
+++ config/flow.ts
@@ -2,7 +2,6 @@ import * as twilioLib from '../lib/twilio'
 const textSystem = new twilioLib.TextSystem()
 const textWalkinSystem = new twilioLib.TextWalkInAppointmentInterface();
 const textBookSystem = new twilioLib.TextBookAppointmentInterface();
-const textAdditionalServices = new twilioLib.TextAdditionalServicesInterface();
 const steps = {
   // This object will contain the appropriate function in respect to the customer's step number
   '1': textSystem.textGetName,
@@ -17,23 +16,13 @@ const walkinSteps = {
 
 const bookSteps = {
   '1': textBookSystem.textChoseApproximateTime,
-  '2': textBookSystem.textChoseExactTime,
-  '3': textBookSystem.textConfirmAppointmentTime,
-  '4': textBookSystem.textGetConfirmation,
-}
-
-const additionalSteps = {
-  '1': textAdditionalServices.textStartAdditionalService,
-  '2': textAdditionalServices.textConfirmAdditionalService
+  '2': textBookSystem.textConfirmAppointmentTime,
+  '3': textBookSystem.textGetConfirmation,
 }
 
 export function processFlow(req, res, next) {
   // This function will call the necessary step based on the customer's step number
   const { appointmentType, stepNumber, finishedGeneralSteps, additionalServices } = req.customer.session
-
-  // check if user would like additional services
-  // If appointment type is not truthy we need to send the user throught the general steps
-  if(additionalServices)  return additionalSteps[stepNumber](req, res, next)
 
   // check if user has finished general steps
   if(appointmentType && finishedGeneralSteps){
