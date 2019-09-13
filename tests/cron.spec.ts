f0de878b202ad756517d2d8559d407f140d4f83a
Nicole Sattler <nicole.satter@live.com>
2019-09-13 14:55:23 -0500

    Set up basic cron fotmatter test

---
+++ tests/cron.spec.ts
@@ -0,0 +1,9 @@
+import * as assert from 'assert'
+import { formatToCronTime } from '../utils'
+
+describe('Cron job time formatter', () => {
+  it('should format dateTime to correct cron format', () => {
+    const result = formatToCronTime('2019-09-13 12:00')
+    assert.equal(result, '0 0 11 13 8 *')
+  })
+})
