5ad376b1bd1ee3685dd1aae12544b4c8b30c3a0b
Nicole Sattler <nicole.satter@live.com>
2019-09-14 12:27:25 -0500

    Remove new date object

--- utils/index.ts
+++ utils/index.ts
@@ -2,17 +2,12 @@ import moment from 'moment'
 
 // Expects time to be in format yyyy-mm-dd hh-mm
 export const formatToCronTime = time => {
-  // Date & Timezone
-  const dateWithTimeZone = new Date().toLocaleString('en-US', {
-    timeZone: 'America/Mexico_City'
-  })
-  const currDate = new Date(dateWithTimeZone)
-
   // Date Values for cron job
   const minutes = moment(time).format('m')
   const hour = moment(time).format('H')
   const alertHour = parseInt(hour) - 1
-  const date = currDate.getDate()
+  const dayOfMonth = moment(time).format('D')
+  const month = parseInt(moment(time).format('M')) - 1
 
-  return `0 ${minutes} ${alertHour} ${date} ${currDate.getMonth()} *`
+  return `0 ${minutes} ${alertHour} ${dayOfMonth} ${month} *`
 }
