f93e98aba013c8ee622cf3584b34bc3ce7a6d47d
Anson Ervin <ansonervin@yahoo.com>
2019-09-20 18:37:20 -0400

    Added icon image

--- script/addBarber.js
+++ script/addBarber.js
@@ -1,10 +1,42 @@
 const request = require('request')
 
 // store deal
-const url = `https://cf7cd00e.ngrok.io/api/createBarber`
-
+const url = `http://localhost:80/api/createBarber`
+// fades of gray
 request.post(url).form({
-    name: 'Idris',
+    name: 'Julian',
+    email: '',
+    unavailabilities: {
+        lunch: { from : '13:00', to: '14:00' },
+        vacations: [
+            { from: '2019-08-17 00:00', to: '2019-08-18 00:00'}
+        ],
+        offDays: [
+            { from: '2019-08-19 00:00', to: '2019-08-20 00:00'}
+        ],
+        unavailableTimes:[
+            { from: '2019-08-16 12:00', to: '2019-08-16 13:00'}
+        ]
+    }
+})
+request.post(url).form({
+    name: 'Anthony',
+    email: '',
+    unavailabilities: {
+        lunch: { from : '13:00', to: '14:00' },
+        vacations: [
+            { from: '2019-08-17 00:00', to: '2019-08-18 00:00'}
+        ],
+        offDays: [
+            { from: '2019-08-19 00:00', to: '2019-08-20 00:00'}
+        ],
+        unavailableTimes:[
+            { from: '2019-08-16 12:00', to: '2019-08-16 13:00'}
+        ]
+    }
+})
+request.post(url).form({
+    name: 'Jimmy',
     email: '',
     unavailabilities: {
         lunch: { from : '13:00', to: '14:00' },
@@ -20,36 +52,71 @@ request.post(url).form({
     }
 })
 
-request.post(url).form({
-    name: 'Kelly',
-    email: '',
-    unavailabilities: {
-        lunch: { from : '13:00', to: '14:00' },
-        vacations: [
-            { from: '2019-08-17 00:00', to: '2019-08-18 00:00'}
-        ],
-        offDays: [
-            { from: '2019-08-19 00:00', to: '2019-08-20 00:00'}
-        ],
-        unavailableTimes:[
-            { from: '2019-08-16 12:00', to: '2019-08-16 13:00'}
-        ]
-    }
-})
+// barberSharp 
+// request.post(url).form({
+//     name: 'Jesse',
+//     email: '',
+//     unavailabilities: {
+//         lunch: { from : '13:00', to: '14:00' },
+//         vacations: [
+//             { from: '2019-08-17 00:00', to: '2019-08-18 00:00'}
+//         ],
+//         offDays: [
+//             { from: '2019-08-19 00:00', to: '2019-08-20 00:00'}
+//         ],
+//         unavailableTimes:[
+//             { from: '2019-08-16 12:00', to: '2019-08-16 13:00'}
+//         ]
+//     }
+// })
 
-request.post(url).form({
-    name: 'Jesse',
-    email: '',
-    unavailabilities: {
-        lunch: { from : '13:00', to: '14:00' },
-        vacations: [
-            { from: '2019-08-17 00:00', to: '2019-08-18 00:00'}
-        ],
-        offDays: [
-            { from: '2019-08-19 00:00', to: '2019-08-20 00:00'}
-        ],
-        unavailableTimes:[
-            { from: '2019-08-16 12:00', to: '2019-08-16 13:00'}
-        ]
-    }
-})
+// request.post(url).form({
+//     name: 'Kelly',
+//     email: '',
+//     unavailabilities: {
+//         lunch: { from : '13:00', to: '14:00' },
+//         vacations: [
+//             { from: '2019-08-17 00:00', to: '2019-08-18 00:00'}
+//         ],
+//         offDays: [
+//             { from: '2019-08-19 00:00', to: '2019-08-20 00:00'}
+//         ],
+//         unavailableTimes:[
+//             { from: '2019-08-16 12:00', to: '2019-08-16 13:00'}
+//         ]
+//     }
+// })
+
+// request.post(url).form({
+//     name: 'Ryan',
+//     email: '',
+//     unavailabilities: {
+//         lunch: { from : '13:00', to: '14:00' },
+//         vacations: [
+//             { from: '2019-08-17 00:00', to: '2019-08-18 00:00'}
+//         ],
+//         offDays: [
+//             { from: '2019-08-19 00:00', to: '2019-08-20 00:00'}
+//         ],
+//         unavailableTimes:[
+//             { from: '2019-08-16 12:00', to: '2019-08-16 13:00'}
+//         ]
+//     }
+// })
+
+// request.post(url).form({
+//     name: 'Steph',
+//     email: '',
+//     unavailabilities: {
+//         lunch: { from : '13:00', to: '14:00' },
+//         vacations: [
+//             { from: '2019-08-17 00:00', to: '2019-08-18 00:00'}
+//         ],
+//         offDays: [
+//             { from: '2019-08-19 00:00', to: '2019-08-20 00:00'}
+//         ],
+//         unavailableTimes:[
+//             { from: '2019-08-16 12:00', to: '2019-08-16 13:00'}
+//         ]
+//     }
+// })
