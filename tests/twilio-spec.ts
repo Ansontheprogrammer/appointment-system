1771a3037ec59391a4cc3577234f5c82a1aef2cd
Anson Ervin <ansonervin@yahoo.com>
2019-12-18 23:37:44 -0500

    Added tests for get available times

--- tests/twilio-spec.ts
+++ tests/twilio-spec.ts
@@ -5,6 +5,7 @@ import { TextSystem } from '../lib/flow/smsFlow/smsFlow'
 import { Database, serviceList } from '../lib/database';
 import sinon from 'sinon';
 import { shopIsClosed } from '../config/utils';
+import { BARBER } from '../lib';
 
 describe('User message interface', () => {
     const sampleServices = [
@@ -191,6 +192,93 @@ Total: $40`
     })
 })
 
+
+describe('scheduling appointments', () => {
+    Database.setBarberShopData({}, {
+        sendStatus: () => {}
+    }, () => {})
+
+    const sampleService = [
+        {
+            service: 'Adult Haircut',
+            price: 25,
+            duration: 45
+        }
+    ]
+
+    const exampleBarber = {
+        phoneNumber: '',
+        email: '',
+        name: '',
+        appointments: [
+            {
+                phoneNumber: '',
+                firstName: '',
+                uuid: '',
+                details: {
+                  services: [],
+                  time: {
+                    from: '2019-15-10 10:00',
+                    to: '2019-15-10 11:00'
+                  },
+                  total: 60
+                }
+            }
+        ],
+        unavailabilities: {
+            lunch: {
+                from: '10:00',
+                to: '10:30'
+            },
+            offDays: [
+                {
+                    from: '2019-12-10',
+                    to: '2019-12-11'
+                }
+            ],
+            vacations: [],
+            unavailableTimes: []
+        }
+    }
+
+    describe('getAvailableTimes', () => {
+        it('should not be able to book an appointment during a lunch time', () => {
+            const availableTimes = twilioLib.getAvailableTimes(30, 15, [], '2019-12-12', '2019-12-13', (exampleBarber as any))
+            const timesThatShouldBeUnavailable = availableTimes.filter(availableTime => availableTime.time === '10:00' || availableTime.time === '10:15');
+            const expectedTimeAvailability = [ 
+                { time: '10:00', available: false, reference: null },
+                { time: '10:15', available: false, reference: null } 
+            ]
+            assert.deepEqual(timesThatShouldBeUnavailable, expectedTimeAvailability)
+        })
+        it('should be able to book an appointment if it is not lunch time or off day', () => {
+            const availableTimes = twilioLib.getAvailableTimes(30, 15, [], '2019-12-12', '2019-12-13', (exampleBarber as any))
+            const timesThatShouldBeUnavailable = availableTimes.filter(availableTime => availableTime.time === '10:30' || availableTime.time === '11:00');
+            const expectedTimeAvailability = [ 
+                { time: '10:30', available: true, reference: null },
+                { time: '11:00', available: true, reference: null } 
+            ]
+            assert.deepEqual(timesThatShouldBeUnavailable, expectedTimeAvailability)
+        })
+    
+        it('should not be able to book an appointment during an off day', () => {
+            const availableTimes = twilioLib.getAvailableTimes(30, 15, [], '2019-12-10', '2019-12-11', (exampleBarber as any))
+            const areAllTimesUnavailable = availableTimes.every(availableTime => availableTime.available === false)
+            assert.equal(areAllTimesUnavailable, true)
+        })
+    })
+    // describe('getBarberAppointments', () => {
+    //     it.only('should not be able to book an appointment during a lunch time', () => {
+    //         const barberAppointments = twilioLib.getBarberAppointments(sampleService, (exampleBarber as any));
+    //         console.log(barberAppointments, 'barberAppointments')
+    //     })
+    //     it('should be able to book an appointment if it is not lunch time or off day', () => {
+    //     })
+    
+    //     it('should not be able to book an appointment during an off day', () => {
+    //     })
+    // })
+})
 
 describe('Text System', () => {
     let sandbox: sinon.SinonSandbox;
