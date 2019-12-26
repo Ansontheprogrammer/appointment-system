558883e29cef5412d00f4c27e2003c6c18c5a9c3
Anson Ervin <ansonervin@yahoo.com>
2019-12-26 16:49:45 -0500

    Improved indention

--- tests/db-spec.ts
+++ tests/db-spec.ts
@@ -31,7 +31,6 @@ describe('Database class', () => {
             console.log(timeZone, 'timeZone')
             assert.equal(timeZone, 'America/Chicago')
         })
-        
     })
 
     describe('addAppointment', () => {
@@ -96,8 +95,8 @@ describe('Database class', () => {
             }, done)
         })
 
-    it('should create an appointment with appointments stored', done => {
-        const expectedAppointments = `{
+        it('should create an appointment with appointments stored', done => {
+            const expectedAppointments = `{
     "appointments": [
         {
             "phoneNumber": "9082097544",
@@ -131,55 +130,55 @@ describe('Database class', () => {
         }
     ]
 }`
-        
-        sandbox.stub(databaseLib.barberCollection, 'doc').callsFake((barberName: string) => { 
-            assert.equal(barberName, 'Julian');
-            return {
-                get: () => {
-                    return {
-                        get: (field: string) => {
-                            assert.equal(field, 'appointments');
-                            return [{
-                                "phoneNumber": "9082097544",
-                                "firstName": "Anson",
-                                "details": {
-                                    "services": [
-                                        "Hair cut"
-                                    ],
-                                    "time": {
-                                        "duration": 30,
-                                        "from": "2019-09-13 13:30"
+            
+            sandbox.stub(databaseLib.barberCollection, 'doc').callsFake((barberName: string) => { 
+                assert.equal(barberName, 'Julian');
+                return {
+                    get: () => {
+                        return {
+                            get: (field: string) => {
+                                assert.equal(field, 'appointments');
+                                return [{
+                                    "phoneNumber": "9082097544",
+                                    "firstName": "Anson",
+                                    "details": {
+                                        "services": [
+                                            "Hair cut"
+                                        ],
+                                        "time": {
+                                            "duration": 30,
+                                            "from": "2019-09-13 13:30"
+                                        },
+                                        "total": 25
                                     },
-                                    "total": 25
-                                },
-                                "uuid": "24a888e0-2824-11ea-9786-1568773a6827"
-                                }
-                            ]
+                                    "uuid": "24a888e0-2824-11ea-9786-1568773a6827"
+                                    }
+                                ]
+                            }
                         }
-                    }
-                },
-                update: (updatedAppointments: BARBER_APPOINTMENTS[]) => {
-                    // assert.equal(JSON.stringify(updatedAppointments, null, 4), expectedAppointment)
-                    assert.equal(JSON.stringify(updatedAppointments, null, 4), expectedAppointments)
-                },
-            }
-        })
-        sandbox.stub(uuid, 'v1').callsFake(() => { 
-            return '24a888e0-2824-11ea-9786-1568773a6827'
-        })
+                    },
+                    update: (updatedAppointments: BARBER_APPOINTMENTS[]) => {
+                        // assert.equal(JSON.stringify(updatedAppointments, null, 4), expectedAppointment)
+                        assert.equal(JSON.stringify(updatedAppointments, null, 4), expectedAppointments)
+                    },
+                }
+            })
+            sandbox.stub(uuid, 'v1').callsFake(() => { 
+                return '24a888e0-2824-11ea-9786-1568773a6827'
+            })
 
-        // TODO: Find a way to override firebase
-        const details = {
-            services: ['Hair cut'],
-            time: { 
-                duration: 30,
-                from: '2019-09-14 13:30'
-            },
-            total: 25
-        }
-        new databaseLib.Database().addAppointment(barberName, customer, details).then(() => {
-            done()
-        }, done)
+            // TODO: Find a way to override firebase
+            const details = {
+                services: ['Hair cut'],
+                time: { 
+                    duration: 30,
+                    from: '2019-09-14 13:30'
+                },
+                total: 25
+            }
+            new databaseLib.Database().addAppointment(barberName, customer, details).then(() => {
+                done()
+            }, done)
+        })
     })
-})
 })
