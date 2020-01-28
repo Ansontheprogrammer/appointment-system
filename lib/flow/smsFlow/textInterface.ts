9a21392da4d6fa84059942f105f7cfa279d2b99f
Anson Ervin <ansonervin@yahoo.com>
2020-01-28 09:49:45 -0500

    Refactored text interface

--- lib/flow/smsFlow/textInterface.ts
+++ lib/flow/smsFlow/textInterface.ts
@@ -10,12 +10,21 @@ import {
 } from '../../twilio'
 
 export class TextInterface {
-    public static getTextMessageTwiml(res: any) {
-        return message => {
-            const msg = new MessagingResponse()
-            msg.message(message)
-            return res.send(msg.toString())
-        }
+    private req; 
+    private res; 
+    private next
+    private textTwiml
+
+    constructor(req, res, next){
+        this.req = req;
+        this.res = res;
+        this.next = this.next;
+    }
+
+    public sendTextMessage(message: string){
+        const msg = new MessagingResponse()
+        this.textTwiml = msg.message(message)
+        return this.res.send(this.textTwiml.toString())
     }
 
     public static userInterfaceOptions = {
@@ -23,52 +32,58 @@ export class TextInterface {
             action: '1',
             name: 'Book Appointment Online'
         },
-        // bookAppointmentOffline: {
-        //     action: '2',
-        //     name: 'Book Appointment through'
-        // },
-        // shopHours: {
-        //     action: '5',
-        //     name: 'Shop Hours'
-        // },
-        cancelAppointment: {
+        bookAppointmentOffline: {
+            action: '2',
+            name: 'Book Appointment through text'
+        },
+        bookFirstAvailableAppointment: {
+            action: '3',
+            name: 'Book First Available Appointment'
+        },
+        shopHours: {
+            action: '4',
+            name: 'Shop Hours'
+        },
+        viewAppointment: {
             action: 'View',
             name: 'View Appointments'
         },
     }
 
-    public static sendInterface(res){
-        const sendTextMessage = TextInterface.getTextMessageTwiml(res)
-        sendTextMessage(UserMessage.generateTextInterfaceMessage());
+    private sendInterface(res){
+        this.sendTextMessage(UserMessage.generateTextInterfaceMessage());
     }
 
-    public static invalidInterfaceOption(res){
-       const sendTextMessage = TextInterface.getTextMessageTwiml(res)
-       sendTextMessage('Sorry that was an invalid option\n')
-       TextInterface.sendInterface(res);
+    private invalidInterfaceOption(res){
+       this.sendTextMessage('Sorry that was an invalid option\n')
+       this.sendInterface(res);
     }
 
-    public async userInterface(req, res, next) {
+
+    public async userInterface() {
         /* Todo:
             Add a feature that they can press maybe 3 to cancel their most recent appointment
             If they press four they can cancel a future appointment 
          */
-        const userMessage: string = extractText(req.body.Body);
-        const phoneNumber = phoneNumberFormatter(req.body.From)
-        const sendTextMessage = TextInterface.getTextMessageTwiml(res);
+        const userMessage: string = extractText(this.req.body.Body);
+        const phoneNumber = phoneNumberFormatter(this.req.body.From)
         try {
             let customer = await database.findCustomerInDatabase(phoneNumber)
             // set req.customer to the customer found in database or an object containing the customer's phone number. 
-            req.customer = !!customer ? customer : { phoneNumber }
+            this.req.customer = !!customer ? customer : { phoneNumber }
         } catch(err) {
             console.error(err, 'Error finding customer');
         }
-        if(userMessage === '1'){
-            return sendBookLaterDateLink(phoneNumber)
-        } else if(userMessage.toLowerCase() === 'view'){
-            return cancelRecentAppointment(req, res);
-        } else {
-            return TextInterface.invalidInterfaceOption(res)
+        switch (userMessage) {
+            case '1':
+                return sendBookLaterDateLink(phoneNumber)
+            case '4': 
+                return this.sendTextMessage('')
+            case 'view':
+                return cancelRecentAppointment(this.req, this.res)
+            default:
+                return this.invalidInterfaceOption(this.res)
+
         }
     }
 }
